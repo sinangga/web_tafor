94ccbc77503115438d4435d8dd47e46a8efb3d4f
Neptusun <70463907+sinangga@users.noreply.github.com>
2024-01-13 15:53:36 +0700

    Update prakiraan.py

--- app/prakiraan.py
+++ app/prakiraan.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import pandas
 tab1 = st.tabs(["XML"])
 
 with tab1:
    st.header("XML")
-   st.write("https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/DigitalForecast-KalimantanBarat.xml")
+   df = pandas.read_xml("https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/DigitalForecast-KalimantanBarat.xml")
+   st.write(df)
    #st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
