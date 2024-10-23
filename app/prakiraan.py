2a5189cd5c850d31b0c4bbb5aa228721be5b17fb
Neptusun <70463907+sinangga@users.noreply.github.com>
2024-10-24 00:30:46 +0700

    Update prakiraan.py

--- app/prakiraan.py
+++ app/prakiraan.py
@@ -45,13 +45,64 @@ def printcuaca(x):
 
 ###
 
-tab1, tab2 = st.tabs(["Prakicu","Test"])
+tab1, tab2 = st.tabs(["Kabupaten","Kecamatan"])
 
 with tab1:
-    st.header("Prakicu")
-    st.write(nesting("Bika"))
+    st.header("Kabupaten")
+    import urllib.request, json
+    import requests
+    import pandas as pd
+    from prettytable import PrettyTable
+    
+    # Bypass Forbidden Status Code
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    
+    # Store Data from BMKG API (Kecamatan / adm2)
+    kecamatan_res = requests.get("https://api.bmkg.go.id/publik/prakiraan-cuaca?adm2=61.06", headers=headers)
+    kecamatan_output = kecamatan_res.json()
+    df_kh = kecamatan_output['data']
+    
+    # Clustering Data into Single Dataset
+    list_kecamatan = []
+    for kh in df_kh:
+        list_kecamatan.append(kh['lokasi']['kecamatan'])
+    
+    #### Main Dataset ####
+    df_kh_2 = dict(zip(list_kecamatan, df_kh)) 
+    ######################
+    
+    # Function to Call Specific Weather Data Kecamatan
+    def nesting(nama_kecamatan):
+        nesting = []
+        for i in df_kh_2[(nama_kecamatan)]["cuaca"]:
+            nesting.append(i)
+        nest1 = nesting[0]
+        nest2 = nesting[1]
+        nest3 = nesting[2]
+        nesting_list = [nest1, nest2, nest3]
+        nesting = [element for nestedlist in nesting_list for element in nestedlist]
+        return nesting
+    
+    # 
+    def printcuaca(x):
+        n = 0
+        t = PrettyTable(['Kecamatan', 'Tanggal', 'Cuaca'])
+        for i in list_kecamatan:
+            #print(list_kecamatan[n])
+            data = nesting(i)
+            for a in range(x):
+                #print(list_kecamatan[n])
+                #print(list_kecamatan[n],data[a]["utc_datetime"], data[a]["weather_desc"])
+                t.add_row([list_kecamatan[n],data[a]["utc_datetime"], data[a]["weather_desc"]])
+                #append()
+            n = n+1
+        return t
+
+    st.write(t)
+
 with tab2:
-    st.header("Test")
+    st.header("Kecamatan")
+    
     st.image("https://satelit.bmkg.go.id/IMAGE/HIMA/H08_EH_Kalbar.png", width=None)
    #df = pandas.read_xml("https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/DigitalForecast-KalimantanBarat.xml")
    #st.write(df)
