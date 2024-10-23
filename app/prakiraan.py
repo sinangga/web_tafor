8736d2482f40af5ae093dcb348654f68257aa158
Neptusun <70463907+sinangga@users.noreply.github.com>
2024-10-24 00:34:06 +0700

    Update prakiraan.py

--- app/prakiraan.py
+++ app/prakiraan.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas
 import urllib.request, json
 import requests
+import prettytable
 
 ###
 # Bypass Forbidden Status Code
@@ -36,12 +37,17 @@ def nesting(nama_kecamatan):
 # 
 def printcuaca(x):
     n = 0
+    t = PrettyTable(['Kecamatan', 'Tanggal', 'Cuaca'])
     for i in list_kecamatan:
-        print(list_kecamatan[n])
+        #print(list_kecamatan[n])
         data = nesting(i)
         for a in range(x):
-            print(data[a]["utc_datetime"], data[a]["weather_desc"])
+            #print(list_kecamatan[n])
+            #print(list_kecamatan[n],data[a]["utc_datetime"], data[a]["weather_desc"])
+            t.add_row([list_kecamatan[n],data[a]["utc_datetime"], data[a]["weather_desc"]])
+            #append()
         n = n+1
+    return t
 
 ###
 
@@ -49,56 +55,7 @@ tab1, tab2 = st.tabs(["Kabupaten","Kecamatan"])
 
 with tab1:
     st.header("Kabupaten")
-    import urllib.request, json
-    import requests
-    import pandas as pd
-    from prettytable import PrettyTable
-    
-    # Bypass Forbidden Status Code
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    
-    # Store Data from BMKG API (Kecamatan / adm2)
-    kecamatan_res = requests.get("https://api.bmkg.go.id/publik/prakiraan-cuaca?adm2=61.06", headers=headers)
-    kecamatan_output = kecamatan_res.json()
-    df_kh = kecamatan_output['data']
-    
-    # Clustering Data into Single Dataset
-    list_kecamatan = []
-    for kh in df_kh:
-        list_kecamatan.append(kh['lokasi']['kecamatan'])
-    
-    #### Main Dataset ####
-    df_kh_2 = dict(zip(list_kecamatan, df_kh)) 
-    ######################
-    
-    # Function to Call Specific Weather Data Kecamatan
-    def nesting(nama_kecamatan):
-        nesting = []
-        for i in df_kh_2[(nama_kecamatan)]["cuaca"]:
-            nesting.append(i)
-        nest1 = nesting[0]
-        nest2 = nesting[1]
-        nest3 = nesting[2]
-        nesting_list = [nest1, nest2, nest3]
-        nesting = [element for nestedlist in nesting_list for element in nestedlist]
-        return nesting
-    
-    # 
-    def printcuaca(x):
-        n = 0
-        t = PrettyTable(['Kecamatan', 'Tanggal', 'Cuaca'])
-        for i in list_kecamatan:
-            #print(list_kecamatan[n])
-            data = nesting(i)
-            for a in range(x):
-                #print(list_kecamatan[n])
-                #print(list_kecamatan[n],data[a]["utc_datetime"], data[a]["weather_desc"])
-                t.add_row([list_kecamatan[n],data[a]["utc_datetime"], data[a]["weather_desc"]])
-                #append()
-            n = n+1
-        return t
-
-    st.write(t)
+    st.write(printcuaca(3))
 
 with tab2:
     st.header("Kecamatan")
