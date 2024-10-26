553fdab1712c660c90c4efc4d44a9c11eb200396
Neptusun <70463907+sinangga@users.noreply.github.com>
2024-10-26 23:59:51 +0700

    Update aktual.py

--- app/aktual.py
+++ app/aktual.py
@@ -3,15 +3,15 @@ import requests
 tab1, tab2, tab3 = st.tabs(["METAR", "SATELIT", "RADAR"])
 
 with tab1:
-   st.header("METAR 24 Jam")
+   st.header("METAR & TAFOR")
    URL = "https://api.bmkg.go.id/cuaca-bandara?icao=WIOP"
 
    r=requests.get(URL, headers={"X-API-KEY": "}Af*4TG=ZGp99sT",
                                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'})
    data = r.json()
    st.write("Bandar Udara Pangsuma Kapuas Hulu")
-   st.write("Last METAR : ", data["last_observation"])
-   st.write("Last TAFOR : ", data["last_forecast"])
+   st.write("Latest METAR : ", data["last_observation"])
+   st.write("Latest TAFOR : ", data["last_forecast"])
    st.divider()
    st.write("Produced by: BMKG Pangsuma Kapuas Hulu")
 
