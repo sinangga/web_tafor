0217e718d4d844fb4e0227aa0038470f0ce4ea69
Neptusun <70463907+sinangga@users.noreply.github.com>
2024-10-28 23:34:51 +0700

    Update prakiraan.py

--- app/prakiraan.py
+++ app/prakiraan.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import urllib.request, json
 import requests
 import pandas as pd
@@ -25,14 +26,9 @@ df_kh_2 = dict(zip(list_kecamatan, df_kh))
 # Function to Call Specific Weather Data Kecamatan
 def nesting(nama_kecamatan):
     nesting = []
-    for i in df_kh_2[(nama_kecamatan)]["cuaca"]:
+    for i in df_kh_2[nama_kecamatan]["cuaca"]:
         nesting.append(i)
-    nest1 = nesting[0]
-    nest2 = nesting[1]
-    nest3 = nesting[2]
-    nesting_list = [nest1, nest2, nest3]
-    nesting = [element for nestedlist in nesting_list for element in nestedlist]
-    return nesting
+    return [element for nestedlist in nesting for element in nestedlist]
 
 # Function to Create Data inside Prettytable 
 def printcuaca(x):
@@ -41,12 +37,12 @@ def printcuaca(x):
     for i in list_kecamatan:
         data = nesting(i)
         for a in range(x):
-            angin = str(data[a]["wd"])+" "+str(round(data[a]["ws"]))+"KT"
-            t.add_row([list_kecamatan[n],data[a]["utc_datetime"], data[a]["weather_desc"],angin, data[a]["t"], data[a]["hu"]])
-        n = n+1
+            angin = str(data[a]["wd"]) + " " + str(round(data[a]["ws"])) + "KT"
+            t.add_row([list_kecamatan[n], data[a]["utc_datetime"], data[a]["weather_desc"], angin, data[a]["t"], data[a]["hu"]])
+        n += 1
     return t
 
-#psu = ['c1 - cn', 'suhu(min-max)', 'RH (min-max)']
+# Function to collect weather data
 def cuaca_gabungan_pagi(n):
     suhu = []
     angin = []
@@ -61,77 +57,21 @@ def cuaca_gabungan_pagi(n):
         rh.append(nesting(n)[i]["hu"])
         list_cuaca.append(nesting(n)[i]["weather_desc"])
         list_tgl.append(nesting(n)[i]["utc_datetime"])
+    
     maxsuhu = max(suhu)
     minsuhu = min(suhu)
     maxrh = max(rh)
     minrh = min(rh)
-    suhu_akhir = str(maxsuhu) + "-" + str(minsuhu)
-    rh_akhir = str(maxrh) + "-" + str(minrh)
+    suhu_akhir = f"{maxsuhu}-{minsuhu}"
+    rh_akhir = f"{maxrh}-{minrh}"
     angin = max(angin)
     arah = statistics.mode(arah)
     return n, list_tgl, list_cuaca, suhu_akhir, rh_akhir, arah, angin
 
-## Calling Data Kecamatan Daily
-def harian_kecamatan(nama):
-    (a, b, c, d, e, f, g) = cuaca_gabungan_pagi(nama)
-    nama = []
-    for x in (a, d, e, f, g):
-        if x in (a,d,e,f,g):
-            nama.append(x)
-    for i in range(len(c)):
-        nama.append(c[i])
-    order_list = [0,5,6,7,8,9,10,11,12,1,2,3,4]
-    nama = [nama[i] for i in order_list]
-    if nama[11] == "SE":
-        nama[11] = 'Tenggara'
-    if nama[11] == "N":
-        nama[11] = 'Utara'
-    if nama[11] == "E":
-        nama[11] = 'Timur'
-    if nama[11] == "W":
-        nama[11] = 'Barat'
-    if nama[11] == "S":
-        nama[11] = 'Selatan'
-    if nama[11] == "NW":
-        nama[11] = 'Barat Laut'
-    if nama[11] == "NE":
-        nama[11] = 'Timur Laut'
-    if nama[11] == "SW":
-        nama[11] = 'Barat Daya'
-    nama[12] = round(nama[12])
-    return nama
-
-## Printing to Web
-(a, b, c, d, e, f, g) = cuaca_gabungan_pagi("Bika")
-tanggal = b[0][8]+b[0][9]+str("/")+b[0][5]+b[0][6]+str("/")+b[0][0]+b[0][1]+b[0][2]+b[0][3]
-jam = []
-for i in range(len(b)):
-    jam.append(b[i][11]+b[i][12])
-
-jamm = ['Kecamatan', jam, 'Suhu', 'Kelembapan', 'Angin', 'Kecepatan']
-jammm = []
-for i in jamm:
-    if type(i) == list:
-        for a in range(len(jam)):
-            jammm.append(jam[a])
-    else:
-        jammm.append(i)
-
-table = PrettyTable(jammm)
-#table.title = "CUACA KABUPATEN KAPUAS HULU TANGGAL "+tanggal
-for i in list_kecamatan:
-    table.add_row(harian_kecamatan(i))
-table.align["Kecamatan"] = "l"
-#print(table)
-
-
-################################################################
-################################################################
-################################################################
-
+# Create combined data for all kecamatan
 datacoba = []
 for i in list_kecamatan:
-    datacoba.append(harian_kecamatan(i))
+    datacoba.append(cuaca_gabungan_pagi(i))
 headers = ['Kecamatan', '12', '15', '18', '21', '00', '03', '06', '09', 'Suhu', 'Kelembapan', 'Angin', 'Kecepatan']
 result_dicts = [dict(zip(headers, values)) for values in datacoba]
 
@@ -144,7 +84,6 @@ status_to_icon = {
     'Hujan Ringan': 'hujan ringan-am.ico',
     'Hujan Petir': 'hujan ringan-am.ico',
     'Cerah Berawan': 'cerah berawan-am.ico',
-    # Add more mappings as necessary
 }
 
 # Loop through each dictionary and replace values in columns 2-9
@@ -156,5 +95,6 @@ for entry in result_dicts:
 # Convert the updated results to a Pandas DataFrame
 df = pd.DataFrame(result_dicts)
 
-# Display the DataFrame as HTML
-HTML(df.to_html(escape=False))
+# Display the DataFrame as HTML in Streamlit
+st.title("Weather Data")
+st.markdown(df.to_html(escape=False), unsafe_allow_html=True)
