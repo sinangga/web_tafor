343fdd57ae434247de09ee883564b4fa3ac0cad3
Neptusun <70463907+sinangga@users.noreply.github.com>
2024-10-29 00:38:25 +0700

    Update prakiraan.py

--- app/prakiraan.py
+++ app/prakiraan.py
@@ -156,8 +156,12 @@ result_dicts = [dict(zip(headers, values)) for values in datacoba]
 status_to_icon = {
     'Cerah': 'https://raw.githubusercontent.com/sinangga/web_tafor/refs/heads/main/icon/cerah-am.png',
     'Hujan Ringan': 'https://raw.githubusercontent.com/sinangga/web_tafor/refs/heads/main/icon/hujan%20ringan-am.png',
-    'Hujan Petir': 'https://raw.githubusercontent.com/sinangga/web_tafor/refs/heads/main/icon/cerah%20berawan-am.png',
+    'Hujan Petir': 'https://raw.githubusercontent.com/sinangga/web_tafor/refs/heads/main/icon/hujan%20petir-am.png',
     'Cerah Berawan': 'https://raw.githubusercontent.com/sinangga/web_tafor/refs/heads/main/icon/cerah%20berawan-am.png',
+    'Hujan Lebat': 'https://raw.githubusercontent.com/sinangga/web_tafor/refs/heads/main/icon/hujan%20lebat-am.png',
+    'Hujan Sedang': 'https://raw.githubusercontent.com/sinangga/web_tafor/refs/heads/main/icon/hujan%20sedang-am.png',
+    'Kabut': 'https://raw.githubusercontent.com/sinangga/web_tafor/refs/heads/main/icon/hujan%20kabut-am.png',
+    'Udara Kabur': 'https://raw.githubusercontent.com/sinangga/web_tafor/refs/heads/main/icon/udara%20kabur-am.png',
 }
 
 # Loop through each dictionary and replace values in columns 2-9
