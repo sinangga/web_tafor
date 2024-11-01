0899dae4f34f99bb9e047a689b9785b0b47d874b
Neptusun <70463907+sinangga@users.noreply.github.com>
2024-11-01 23:29:10 +0700

    Update prakiraan.py

--- app/prakiraan.py
+++ app/prakiraan.py
@@ -195,71 +195,17 @@ chart = alt.Chart(KH_map).mark_geoshape()
 tab1, tab2 = st.tabs(["Kabupaten","Kecamatan"])
 
 with tab1:
-    tab3, tab4, tab5 = st.tabs([tanggal,'Hari Kedua', 'tes'])
+    tab3, tab4 = st.tabs([tanggal,'Hari Kedua'])
     with tab3:
         #st.header("Kabupaten | Tanggal "+tanggal)
         st.markdown(df.to_html(escape=False), unsafe_allow_html=True)
+        display = df.to_html(escape=False), unsafe_allow_html=True
         st.download_button(
             label="Download data as HTML",
             data=display,
             file_name='output.html',
             mime='text/html',
             )
-    with tab5:
-        st.title('🎈 Image Table Demo')
-
-        df = pd.DataFrame(
-            [
-                [2768571, 130655, 1155027, 34713051, 331002277],
-                [1448753, 60632, 790040, 3070447, 212558178],
-                [654405, 9536, 422931, 19852167, 145934619],
-                [605216, 17848, 359891, 8826585, 1379974505],
-                [288477, 9860, 178245, 1699369, 32969875],
-            ],
-            columns=[
-                "Total Cases",
-                "Total Deaths",
-                "Total Recovered",
-                "Total Tests",
-                "Population",
-            ],
-        )
-        
-        # Create a list named country to store all the image paths
-        country = [
-            "https://www.countries-ofthe-world.com/flags-normal/flag-of-United-States-of-America.png",
-            "https://www.countries-ofthe-world.com/flags-normal/flag-of-Brazil.png",
-            "https://www.countries-ofthe-world.com/flags-normal/flag-of-Russia.png",
-            "https://www.countries-ofthe-world.com/flags-normal/flag-of-India.png",
-            "https://www.countries-ofthe-world.com/flags-normal/flag-of-Peru.png",
-        ]
-        # Assigning the new list as a new column of the dataframe
-        df["Country"] = country
-        
-        # Converting links to html tags
-        def path_to_image_html(path):
-            return '<img src="' + path + '" width="60" >'
-        
-        @st.cache
-        def convert_df(input_df):
-             # IMPORTANT: Cache the conversion to prevent computation on every rerun
-             return input_df.to_html(escape=False, formatters=dict(Country=path_to_image_html))
-        
-        html = convert_df(df)
-        
-        st.markdown(
-            html,
-            unsafe_allow_html=True
-        )
-        
-        # Saving the dataframe as a webpage
-        
-        st.download_button(
-             label="Download data as HTML",
-             data=html,
-             file_name='output.html',
-             mime='text/html',
-         )
 
 with tab2:
     st.header("Kecamatan")
