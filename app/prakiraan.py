570524d5075dc2eaca3ba2936b4a9b91abcbb1dd
Neptusun <70463907+sinangga@users.noreply.github.com>
2024-11-02 23:37:02 +0700

    Update prakiraan.py

--- app/prakiraan.py
+++ app/prakiraan.py
@@ -200,7 +200,7 @@ with tab1:
     tab3, tab4, tab5 = st.tabs([tanggal,'Hari Kedua', 'Unduh Tabel'])
     with tab3:
         #st.header("Kabupaten | Tanggal "+tanggal)
-        st.markdown(df.to_html(index = False, escape=False, unsafe_allow_html=True)
+        st.markdown(df.to_html(index = False, escape=False, unsafe_allow_html=True))
 
 
 with tab2:
