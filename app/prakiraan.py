a5350d2f576bd0d16a0a98d22254106ae5de9f95
Neptusun <70463907+sinangga@users.noreply.github.com>
2024-11-18 21:24:38 +0700

    Update prakiraan.py

--- app/prakiraan.py
+++ app/prakiraan.py
@@ -365,7 +365,7 @@ with tab1:
                 temp_html_file.flush()
                 
                 # Render the HTML as an image
-                hti.screenshot(html_file=temp_html_file.name, save_as=output_file, size=(1080, 1400))
+                hti.screenshot(html_file=temp_html_file.name, save_as=output_file, size=(1080, 1800))
         
         # Add a button for image download
         if st.button("Unduh"):
