20c3977c323f3901823b3e13928f9deb449489b8
Neptusun <70463907+sinangga@users.noreply.github.com>
2024-12-11 17:11:07 +0700

    Create ibf.py

---
+++ app/ibf.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from PIL import Image, ImageDraw
+import requests
+from io import BytesIO
+
+
+tab1, tab2, tab3 = st.tabs(["H0", "H24", "H48"])
+st.write("Impact Based Forecast Wilayah Kalimantan Barat")
+
+with tab1:
+    st.header("H-0")
+    H00 = "https://web-meteo.bmkg.go.id/media/data/bmkg/ibfnew/20_kalbar_00.png"  
+    st.iamge(H00)
+
+with tab2:
+    st.header("H-24")
+    H24 = "https://web-meteo.bmkg.go.id/media/data/bmkg/ibfnew/20_kalbar_24.png"  
+    st.iamge(H24)
+
+with tab3:
+    st.header("H-48")
+    H48 = "https://web-meteo.bmkg.go.id/media/data/bmkg/ibfnew/20_kalbar_48.png"  
+    st.iamge(H48)
